052d1f496e9742dfcdb33c23e2c525c8f9f21aba
gnuhub <gnuhub@gmail.com>
2023-09-27 15:05:34 +0800

    Update install.sh

--- ci/1021-docker-build-www/files/install.sh
+++ ci/1021-docker-build-www/files/install.sh
@@ -18,5 +18,24 @@ env
 echo "============================================================================"
 # TODO HERE
 
+cd ~
+git clone https://github.com/nvm-sh/nvm.git .nvm
+cd .nvm
+git checkout v0.39.5
+
+cd ~
+. ~/.nvm/nvm.sh
+nvm install v14.21.3
+nvm use v14.21.3
+npm install yarn -g
+npm install meteor -g
+export PATH=$HOME/.meteor:$PATH
+echo $PATH
+which node
+node --version
+set -x
+
+npm  config set -g python /usr/bin/python2
+yarn config set python /usr/bin/python2 -g
 
 echo "============================================================================"
